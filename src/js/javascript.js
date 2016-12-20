56d69bfebd5530d089d586ae958501dcd8f4ba53
BeSaRa <besara@windowslive.com>
2016-12-20 23:11:29 +0300

    finish BLOG section;

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -6,13 +6,49 @@
             self.classObject = {
                 hasScroll : 'has-scroll'  // this class added to the body when the page has scroll at lest 1 px from Y
             };
-
+        /**
+         * play slider for our member section
+         */
+        self.playMemberSlider = function(){
+            var membersList = $('#members-list');
+                membersList.length &&
+                membersList.owlCarousel({
+                    loop:true,
+                    margin:10,
+                    nav:false,
+                    items:5,
+                    autoplay:true,
+                    autoplayTimeout:7000
+                });
+        };
+        /**
+         * play slider for blog post
+         */
+        self.playBlogSlider = function(){
+            var blogList = $('#blog-list');
+            blogList.length &&
+            blogList.owlCarousel({
+                    loop:true,
+                    margin:10,
+                    nav:true,
+                    items:4 ,
+                    dots:false ,
+                    autoplay:true
+                });
+        };
+        /**
+         * to remove from body class if exists
+         * @param cls
+         */
         self.removeBodyClass = function(cls){
             var pos = self.bodyClass.indexOf(cls);
             if(pos != -1 )
                 self.bodyClass.splice(pos,1) && $('body').removeClass(cls);
         };
-
+        /**
+         * function to add to body class if not exists
+         * @param cls
+         */
         self.addBodyClass = function(cls){
             // more ensure
             (self.bodyClass.indexOf(cls) == -1 ?  self.bodyClass.push(cls) : false) ? $('body').addClass(cls) : null;
@@ -42,20 +78,16 @@
                 self.changeNav(false);
             }
         };
+        self.checkWindowResize = function(){
+            $(window).on('resize scroll',self.checkNav);
+        };
         /**
          * init the theme scripts
          */
         self.init = function(){
-            $(window).on('resize scroll',self.checkNav);
-
-            var membersList = $('#members-list');
-            if(membersList.length )
-                membersList.owlCarousel({
-                    loop:true,
-                    margin:10,
-                    nav:false,
-                    items:5
-                });
+            self.playBlogSlider();
+            self.checkWindowResize();
+            self.playMemberSlider();
         };
     }
 
